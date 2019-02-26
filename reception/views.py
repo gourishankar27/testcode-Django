56c99aa6b174993360085763198e9de663944486
gourishankar <gourishankar2710@gmail.com>
2019-02-26 14:23:41 +0530

    change in reception format. hos_id added to admin

--- reception/views.py
+++ reception/views.py
@@ -3,10 +3,10 @@
 # Create your views here.
 
 from django.shortcuts import render
-from .models import reception
+from .models import patient
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h2> This page belongs to receptionist</h2>")
+    return HttpResponse("<h2> This page belongs to receptionist . Receptionist will add patient</h2>")
