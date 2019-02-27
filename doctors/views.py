7e2ab9656f0722f2e2dd20e15b15047115f5b31d
pandykad <47741444+pandykad@users.noreply.github.com>
2019-02-27 19:57:37 +0530

    updated views

--- doctors/views.py
+++ doctors/views.py
@@ -1,20 +1,38 @@
+from django.http import HttpResponse
+from .models import Docs, Case
+from django.shortcuts import render
+#from django.http import HttpResponse
+from django.http import Http404
+#from django.template import loader
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
 from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView
-from .models import PatientData
+#from .forms import UserForm
 
-class IndexView(generic.listView)
-    template_name = 'doctors/index.html'
-    context_object_name = 'all_patients'
 
-    def get_queryset(self):
-        return docs.objects.all()
+def index(request):
+    template = 'doctors/add-patientdetails.html'
+    context ={}
 
-class AddCase(request):
-    context = {'PatientData'}
-    template_name = 'doctors/patient_view.html'
+    return render(request,template,context) cvdf
 
-class UpadtePatient(generic.UpdateView):
-    model = PatientData
-    fields = ['patient_caseName','patient_caseInfo','patient_relatedDoc']
-    template_name = 'doctors/addPatient.html'
-    
+#   context_object_name = 'all_patients'\
+
+#
+#   def get_queryset(self):
+ #       return docs.objects.all()
+
+#def AddCaseView():
+    #model = docs
+    #    template_name = 'doctors/patient_view.html'
+
+#def AppendCaseView():
+    #    model = docs
+#  template_name = 'doctors/patient_view.html'
+
+
+#def ViewCaseView():
+    #   model = docs
+    #fields = ['patient_caseName','patient_caseInfo','patient_relatedDoc']
+    #template_name = 'doctors/addPatient'
