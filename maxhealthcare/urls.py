9778fd9aafde89a75690100ff16a60455041a338
gourishankar <gourishankar2710@gmail.com>
2019-02-27 17:06:37 +0530

    administartion app modified

--- maxhealthcare/urls.py
+++ maxhealthcare/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls import url,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reception.urls')),
+    path('', include('administration.urls'))
 ]
