112bca05263fd3a92745ae696a98822dd2051849
gourishankar <gourishankar2710@gmail.com>
2019-02-27 17:12:59 +0530

    files merged

--- max/administration/views.py
+++ max/administration/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from .models import Doctor,Receptionist,Admin
 
 from django.http import HttpResponse
@@ -7,4 +6,3 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "administration/index.html")
-
