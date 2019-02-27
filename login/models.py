3cd12a9a6ea885b344532be3963d950adda5d582
gourishankar <gourishankar2710@gmail.com>
2019-02-27 18:54:58 +0530

    login app created

---
+++ login/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class login(models.Model):
+    loginId = models.CharField(max_length = 100)
+    passWord = models.CharField(max_length = 100)
+    userType = models.CharField(max_length = 30)
