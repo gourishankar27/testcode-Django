5b2e06a040c0a269b885744222eba02dae576b65
ketansingrole <singroleketan@gmail.com>
2019-02-27 19:30:38 +0530

    backend of reception

--- reception/views.py
+++ reception/views.py
@@ -9,10 +9,25 @@ def index(request):
 
 def createPatient(request):
     try:
-        var = request.GET.get('firstname')
-        print(var)
+        firstName = request.GET.get('firstName')
+        lastName = request.GET.get('lastName')
+        mobile = request.GET.get('mobile')
+        caseNumber = request.GET.get('caseNumber')
+        doctorID = request.GET.get('doctorID')
+        age = request.GET.get('age')
+        emailID = request.GET.get('emailID')
+        weight = request.GET.get('weight')
+        bodyMassIndex = request.GET.get('bodyMassIndex')
+        dateOfBirth = request.GET.get('dateOfBirth')
+        address = request.GET.get('address')
+        getsex = request.GET.get('sex')
+        sex='No value set'
+        if(getsex=='1'):
+            sex = 'Male'
+        elif(getsex=='0'):
+            sex = 'Female'       
         
     except:
         Exception
 
-    return HttpResponse("HI " + var)
+    return HttpResponse("" + firstName + "\n"+lastName+"\n"+mobile+"\n"+caseNumber+"\n"+doctorID+"\n"+age+"\n"+emailID+"\n"+weight+"\n"+bodyMassIndex+"\n"+dateOfBirth+" "+address+" "+" "+sex)
