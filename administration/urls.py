4afcb35326f97fb231e3aecb993079e5f5fab337
gourishankar <gourishankar2710@gmail.com>
2019-02-28 19:56:33 +0530

    .

--- administration/urls.py
+++ administration/urls.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from django.urls import include,path
 from django.conf.urls import url,include
+from . import views
+
+app_name = 'administration'
+
+urlpatterns = [
+    
+    url(r'^$', views.index, name='administrations_index'),
+]
