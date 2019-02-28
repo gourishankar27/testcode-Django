7fe62914fa8fc63b53049ac796cf6810bb7c3e33
pandykad <47741444+pandykad@users.noreply.github.com>
2019-02-28 11:48:15 +0530

    Change in doctors models

--- doctors/models.py
+++ doctors/models.py
@@ -3,7 +3,7 @@ import
 
 
 
-class docs(models.Model):
+class Docs(models.Model):
   RecepID = models.Charfield(max_length='20')
   DocsID = models.Charfield(max_lenght='20')
   HospitalID = models.Charfield(max_lenght='20')
@@ -11,7 +11,7 @@ class docs(models.Model):
   def __str__(self):
     return self.RecepID +'-'+ self.DocsID +'-'+ self.HospitalID
 
-class case(models.Model):
+class Case(models.Model):
   docs = models.ForeignKey(docs, on_delete=models.CASCADE)
   CaseNumber = models.CharField()
   CaseName = models.Charfield(max_lenght='20')
