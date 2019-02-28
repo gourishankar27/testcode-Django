9f7231c1f6956d822c4cfb95514e5915657e80e5
pandykad <47741444+pandykad@users.noreply.github.com>
2019-02-28 19:17:58 +0530

    doctors setting added

--- maxhealthcare/settings.py
+++ maxhealthcare/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'doctors.apps.DoctorsConfig'
     'login.apps.LoginConfig',
     'administration.apps.AdministrationConfig',
     'reception.apps.ReceptionConfig',
