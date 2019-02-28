125fdd1924190210be0e38d35c9f99c8ff4f6908
gourishankar <gourishankar2710@gmail.com>
2019-02-28 19:13:15 +0530

    .

--- doctors/models.py
+++ doctors/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-import 
 
 
 
 class docs(models.Model):
-  RecepID = models.Charfield(max_length='20')
-  DocsID = models.Charfield(max_lenght='20')
-  HospitalID = models.Charfield(max_lenght='20')
+  RecepID = models.CharField(max_length=20)
+  DocsID = models.CharField(max_length=20)
+  HospitalID = models.CharField(max_length=20)
 
   def __str__(self):
     return self.RecepID +'-'+ self.DocsID +'-'+ self.HospitalID
@@ -14,8 +13,8 @@ class docs(models.Model):
 class case(models.Model):
   docs = models.ForeignKey(docs, on_delete=models.CASCADE)
   CaseNumber = models.CharField()
-  CaseName = models.Charfield(max_lenght='20')
-  CaseInfo = models.CharField(max_length='250')
+  CaseName = models.CharField(max_length=20)
+  CaseInfo = models.CharField(max_length=250)
   DocUploads = models.FileField()
 
   def __str__(self):
