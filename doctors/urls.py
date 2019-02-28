ea606682e0d1b75be46300cf0abe21b6671db376
pandykad <47741444+pandykad@users.noreply.github.com>
2019-02-28 11:52:16 +0530

    change in models od doctors

--- doctors/urls.py
+++ doctors/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='doctors_index'),
     # url(r'^add_(?P<patient_ID>[0-9]+)/$', views.AddCaseView, name='add_case'),
     #url(r'^view_(?P<patient_ID>[0-9]+)/$', views.ViewCaseView, name='view_case'),
-    #url(r'^add_(?P<patient_ID>[0-9]+)/$', views.det, name='append_case'),
+    #url(r'^ad_(?P<patient_ID>[0-9]+)/$', views.det, name='append_case'),
 ]
