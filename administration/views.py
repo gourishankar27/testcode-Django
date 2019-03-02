eb55a72c8ebd939a3c6bf48e2addb3a786b346e2
Ameya Kale <Ameya@Ameyas-MacBook-Pro-6.local>
2019-03-03 04:21:45 +0530

    patient

--- administration/views.py
+++ administration/views.py
@@ -24,11 +24,11 @@ def createDoctor(request):
         doc_dep = request.GET.get('department')
         doc_exp = request.GET.get('year_exp')
         
-        sex='No value set'
+        '''sex='No value set'
         if(doc_sex == '1'):
             sex = 'Male'
         elif(doc_sex == '0'):
-            sex = 'Female'
+            sex = 'Female' '''
 
         doc = Doctor()
         doc.doc_name = doc_name
@@ -41,7 +41,7 @@ def createDoctor(request):
         doc.doc_sex = doc_sex
         doc.doc_dep = doc_dep
         doc.doc_exp = doc_exp
-        doc.save() 
+        doc.save()   
 
         print(doc.name)
         return HttpResponse("CreateDoctor success")
