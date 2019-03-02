66523be80cbd7267a4d8c9f1de9f4a38097b4058
gourishankar <gourishankar2710@gmail.com>
2019-03-02 19:09:11 +0530

    dfnhud

--- login/views.py
+++ login/views.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
 from django.shortcuts import render
 from .models import login
 from django.http import HttpResponse
@@ -38,44 +34,4 @@ def loginusers(request):
         return HttpResponse("")
     except:
         print(Exception)
-=======
->>>>>>> 037380fdfde157c8ebf4ade23f916f4625587410
-from django.shortcuts import render
-from .models import login
-from django.http import HttpResponse
-from dummy.views import index
-from reception.views import index
 
-
-def index(request):
-    return render(request, "login/login.html")
-
-def loginusers(request):
-    try:
-        username = request.GET.get('username')
-        password = request.GET.get('pass')
-        #print(""+username+"\n"+password)
-        print(username)
-        print(password)
-        print()
-        logintype = login.objects.all()        
-        for types in logintype:
-            if(str(types.loginId) == str(username) and str(types.passWord) == str(password)):
-                print(types.userType)
-                print(types.loginId)
-                #return HttpResponse(""+types.userType)
-                if(types.userType == 'reception'):
-                    return render(request,"reception/reception.html")
-                elif(types.userType == 'patient'):
-                    return render(request,"dummy/login.html")
-                
-            #else:
-             #   return HttpResponse("User not found")
-        return HttpResponse("")
-    except:
-        print(Exception)
-
-<<<<<<< HEAD
-=======
->>>>>>> 9559fc8dc1256365c5d111768bc1e101292dc1cd
->>>>>>> 037380fdfde157c8ebf4ade23f916f4625587410
