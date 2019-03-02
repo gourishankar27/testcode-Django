323716415434bb1c299007f037fce6f0dc084bfc
Ameya Kale <Ameya@Ameyas-MacBook-Pro-6.local>
2019-03-03 04:38:42 +0530

    patients

--- patient/urls.py
+++ patient/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from patient.views import pat_display
+from patient.views import pat_display, index
 
 urlpatterns = [
     #url(r'^$', views.pat_diplsay, name="patient"),
     url(r'^patient/' , pat_display),
+    url(r'^index_patient', index),
 ]
--- patient/views.py
+++ patient/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from doctors.models import Case
+from reception.models import patient
 from doctors import views
 
-# Create your models here.
-
-#pat_data = patient()
+def index(request):
+    return render(request, 'patient/index.html')
 
 def pat_display(request):
     pat_data = patient.objects.all()
     context = {
         'pat_data': pat_data
     }
-    return render(request, "patient/index.html")
+    return render(request, "patient/sample.html", context)
 
     #pat_data = patient()
     #pat_name = pat_data.pat_name
