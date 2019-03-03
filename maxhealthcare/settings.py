4479c2df0aa96e006dd2dd56513ec2324e2f7207
Shivani Dere <shivanidere@outlook.com>
2019-03-03 14:45:37 +0530

    .

--- maxhealthcare/settings.py
+++ maxhealthcare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0))5i+u9$ec0r9*4!o_k5p8k)y+bule0g03-b2fvfz(=+_^g5h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.137.1','localhost']
 
 
 # Application definition
